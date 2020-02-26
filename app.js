60608d478949e00f7ebe78f1f6d71f5eea190212
njhom <giantlefty99@gmail.com>
2020-02-25 21:07:11 -0800

    updates?

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-window.onload = function() {
-  Particles.init({
-    selector: '.background'
-  });
-};
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
